abb459937742c106e09e290fb58cccf57e081816
nikita-57 <nikitaprimi@gmail.com>
2024-08-15 16:24:09 +0300

    second update

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import logging
 
 # Укажи свой токен напрямую
 TOKEN = "6782021076:AAGy-0Hn-PWIpviHnOHx_slOllQSA9-E3Yw"
+ADMIN_ID = "620210761"  # Укажи ID администратора (например, свой Telegram ID)
 
 # Создание объекта бота и диспетчера
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
+# Включение логирования для отладки
+logging.basicConfig(level=logging.INFO)
 
 # Создание кнопок для выбора курса
 inline_kb_courses = InlineKeyboardMarkup(row_width=1)  # Кнопки будут расположены в один столбец
 button1 = InlineKeyboardButton('Курс 1: Python для начинающих', callback_data='course1')
 button2 = InlineKeyboardButton('Курс 2: Продвинутый Python', callback_data='course2')
 button3 = InlineKeyboardButton('Курс 3: Веб-разработка на Django', callback_data='course3')
-inline_kb_courses.add(button1, button2, button3)
+button4 = InlineKeyboardButton('Курс 4: Сетевой инженер', callback_data='course4')
+inline_kb_courses.add(button1, button2, button3, button4)
 
 # Кнопки для дополнительной логики
 def get_course_options_markup(course_id):
@@ -51,6 +57,8 @@ async def process_course_selection(callback_query: types.CallbackQuery):
         course_name = 'Продвинутый Python'
     elif course_id == 'course3':
         course_name = 'Веб-разработка на Django'
+    elif course_id == 'course4':
+        course_name = 'Сетевой инженер'  # Исправлено присвоение значения
 
     await bot.send_message(callback_query.from_user.id, f'Ты выбрал курс: {course_name}')
 
@@ -70,8 +78,26 @@ async def process_additional_options(callback_query: types.CallbackQuery):
         # Показываем подробную информацию о курсе
         await bot.send_message(callback_query.from_user.id, f'Подробная программа курса {course_id}: ...\n(здесь может быть детальное описание программы)')
     elif action == 'enroll':
-        # Реализуем логику записи на курс
-        await bot.send_message(callback_query.from_user.id, f'Вы успешно записались на курс {course_id}!\nНаш менеджер свяжется с вами в ближайшее время.')
+        # Отправляем сообщение админу
+        user_info = callback_query.from_user
+        course_name = ""
+        if course_id == 'course1':
+            course_name = 'Python для начинающих'
+        elif course_id == 'course2':
+            course_name = 'Продвинутый Python'
+        elif course_id == 'course3':
+            course_name = 'Веб-разработка на Django'
+        elif course_id == 'course4':
+            course_name = 'Сетевой инженер'  # Исправлено присвоение значения
+
+        admin_message = (
+            f"Пользователь {user_info.full_name} (@{user_info.username}) хочет записаться на курс: {course_name}.\n"
+            f"ID пользователя: {user_info.id}"
+        )
+        await bot.send_message(ADMIN_ID, admin_message)
+
+        # Подтверждаем пользователю
+        await bot.send_message(callback_query.from_user.id, f'Вы успешно записались на курс {course_name}!\nНаш менеджер свяжется с вами в ближайшее время.')
 
     await bot.answer_callback_query(callback_query.id)
 
